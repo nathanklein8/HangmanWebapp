551af57fb73cea795085ffc1da629d1f0c5c0d45
Nathan Klein <neklein3@gmail.com>
2024-08-03 13:18:46 -0400

    initial

---
+++ lib/hungmen.tsx
@@ -0,0 +1,13 @@
+export const dead = () => {
+  return (
+    <p className="text-center m-10">
+      _________ <br></br>
+      |&emsp;&ensp;|&emsp;&ensp;|<br></br>
+      |&emsp;&nbsp;O&emsp;&nbsp;|<br></br>
+      |&ensp;&ensp;&nbsp;/|\&ensp;&ensp;&nbsp;|<br></br>
+      |&emsp;&nbsp;/ \&emsp;&nbsp;| <br></br>
+      |&emsp;&emsp;&emsp;&ensp;|
+    </p>
+  )
+}
+
